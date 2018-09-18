e918135558f4a3eb07457959913f4b88ac85b030
JiahongZhou <JoeZJiahong@gmail.com>
2018-09-18 17:36:58 +0800

    ADD: get_top_k_indexes_of_list

---
+++ general/list_utils.py
@@ -0,0 +1,28 @@
+"""
+@author Joe
+
+This file contains some pretty functions
+"""
+
+
+def get_top_k_indexes_of_list(target_list, k, is_max=True):
+    """
+    get the top k indexes of elements in list
+
+    Example:
+    Problem: I have a list say a = [5,3,1,4,10],
+            and I need to get a index of top two values of the list viz 5 and 10.
+            Is there a one-liner that python offers for such a case?
+    Usage: get_top_k_indexes_of_list(target_list=a, k=2, is_max=True)
+
+    link: https://stackoverflow.com/questions/13070461/get-index-of-the-top-n-values-of-a-list-in-python
+
+    :param target_list: target list
+    :param k: the number of indexes
+    :param is_max: True means max else False means min
+    :return: a list of indexes
+    """
+    indexes = sorted(range(len(target_list)), key=lambda i: target_list[i], reverse=is_max)[:k]
+    return indexes
+
+
